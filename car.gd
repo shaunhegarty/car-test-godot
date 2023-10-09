b7707c341bff163b38b6085e2a23a26dbb6fb7e3
shaun <shaunhegarty@gmail.com>
2023-10-09 21:39:32 +0100

    Refactor car behaviour control to car class
    - UI now only asks car for information or sets information
    - Behaviour like handbrake handled by car

--- car.gd
+++ car.gd
@@ -1,13 +1,24 @@
 class_name Car extends VehicleBody3D
 
 signal reset_car(p_transform: Transform3D )
-signal handbrake(p_active: bool )
+signal values_updated
+
+# Vehicle Parts
+@export var front_wheels: Array[VehicleWheel3D] = []
+@export var rear_wheels: Array[VehicleWheel3D] = []
+
 
 var acceleration_speed = 2500
 var BRAKE_FORCE = 20
 
+var is_braking = false
+var is_handbraking = false
+var grip_balance = 0.5
+var friction_slip = 1.0
+
 var resetting = false
 var reset_transform: = Transform3D()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,6 +29,101 @@ func reset_transform_to_initial(target_transform: Transform3D):
 	resetting = true
 	reset_transform = target_transform
 
+
+func set_wheel_rest_length(p_length: float ):
+	for wheel in front_wheels + rear_wheels:
+		wheel.wheel_rest_length = p_length
+
+
+func set_wheel_roll_influence(p_influence: float ):
+	for wheel in front_wheels + rear_wheels:
+		wheel.wheel_roll_influence = p_influence
+	
+	
+func set_suspension_stiffness(p_stiffness: float ):
+	for wheel in front_wheels + rear_wheels:
+		wheel.suspension_stiffness = p_stiffness
+
+
+func set_suspension_max_force(p_force: float ):
+	for wheel in front_wheels + rear_wheels:
+		wheel.suspension_max_force = p_force
+
+
+func set_suspension_travel(p_travel: float ):
+	for wheel in front_wheels + rear_wheels:
+		wheel.suspension_travel = p_travel
+
+
+func set_damping_compression(p_damping_compression: float ):
+	for wheel in front_wheels + rear_wheels:
+		wheel.damping_compression = p_damping_compression
+
+
+func set_damping_relaxation(p_damping_relaxation: float ):
+	for wheel in front_wheels + rear_wheels:
+		wheel.damping_relaxation = p_damping_relaxation
+
+
+func pull_handbrake():
+	is_handbraking = true
+	for wheel in rear_wheels:
+		wheel.brake = BRAKE_FORCE * 2
+	set_wheel_friction()
+
+
+func release_handbrake():
+	is_handbraking = false
+	for wheel in rear_wheels:
+		wheel.brake = 0
+	set_wheel_friction()
+
+
+func set_grip_balance(p_grip_balance: float):
+	grip_balance = clamp(p_grip_balance, 0, 1)
+	set_wheel_friction()
+
+func set_friction_slip(p_friction_slip: float):
+	print("setting friction slip from %.1f to: %.1f "% [friction_slip, p_friction_slip])
+	friction_slip = p_friction_slip
+	set_wheel_friction()
+
+func set_wheel_friction():
+	var forward_balance = get_forward_balance()
+	var rear_balance = get_rear_balance()
+	if is_handbraking:
+		forward_balance = 1
+		rear_balance = 0.1
+
+	for wheel in front_wheels:
+		wheel.wheel_friction_slip = friction_slip * forward_balance
+	
+	for wheel in rear_wheels:
+		wheel.wheel_friction_slip = friction_slip * rear_balance
+	
+	values_updated.emit()
+	
+
+
+func get_forward_balance() -> float:
+	var grip_balance_to_use = grip_balance
+	if is_handbraking:
+		grip_balance_to_use = 0.1
+	
+	if grip_balance_to_use > 0:
+		return min(grip_balance_to_use, 0.5) / grip_balance_to_use
+	else:
+		return 1
+	
+func get_rear_balance() -> float:
+	var grip_balance_to_use = grip_balance
+	if is_handbraking:
+		grip_balance_to_use = 0.1
+		
+	if 1 - grip_balance_to_use != 0:
+		return min(1 - grip_balance_to_use, 0.5) / (1 - grip_balance_to_use)
+	else:
+		return 1
 
 
 func _integrate_forces(state):
@@ -42,6 +148,7 @@ func _unhandled_input(event):
 	
 	if event.is_action_pressed("brake"):
 		if linear_velocity.length() > 1:
+			is_braking = true
 			brake = BRAKE_FORCE
 		else:
 			engine_force = -acceleration_speed
@@ -49,9 +156,11 @@ func _unhandled_input(event):
 		brake = 0
 	
 	if event.is_action_pressed("handbrake"):
-		handbrake.emit(true)
+		pull_handbrake()
+		set_wheel_friction()
 	if event.is_action_released("handbrake"):
-		handbrake.emit(false)
+		is_handbraking = false
+		set_wheel_friction()
 	
 	if event.is_action_pressed("ui_cancel") or event.is_action_pressed("ui_accept"):
 		var focus_owner = get_viewport().gui_get_focus_owner()
--- car_stat_controller.gd
+++ car_stat_controller.gd
@@ -23,13 +23,9 @@ extends CanvasLayer
 # Vehicle Reset
 @export var reset_button: Button
 
-# Vehicle Parts
-@export var front_wheels: Array[VehicleWheel3D] = []
-@export var rear_wheels: Array[VehicleWheel3D] = []
 
 # Vehicle Initial State
 var initial_car_position: Transform3D
-var is_handbraking = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -47,15 +43,10 @@ func _ready():
 	damping_relaxation_spinner.value_changed.connect(update_wheels)
 	
 	reset_button.pressed.connect(reset_car_position)
-	vehicle.handbrake.connect(set_handbrake)
+	vehicle.values_updated.connect(update_balance_labels)
 
 	update_wheels()
 	update_acceleration()
-
-
-func set_handbrake(_is_handbraking):
-	is_handbraking = _is_handbraking
-	update_wheels()
 
 
 func update_acceleration(_update_value=null):
@@ -63,41 +54,29 @@ func update_acceleration(_update_value=null):
 
 
 func update_wheels(_update_value=null):
-	for wheel in front_wheels + rear_wheels:
-		wheel.wheel_rest_length = rest_spinner.value
-		wheel.wheel_roll_influence = roll_influence_spinner.value
-		wheel.suspension_stiffness = stiffness_spinner.value
-		wheel.suspension_max_force = max_force_spinner.value
-		wheel.suspension_travel = travel_spinner.value
-		wheel.damping_compression = damping_compression_spinner.value
-		wheel.damping_relaxation = damping_relaxation_spinner.value
-	
-	var forward_balance: float
-	var rear_balance: float
-	
-	if is_handbraking:
-		forward_balance = 1
-		rear_balance = 0.1
-	else:	
-		forward_balance = min(balance_slider.value, 0.5) / balance_slider.value
-		rear_balance = min(1 - balance_slider.value, 0.5) / (1 - balance_slider.value)
+	vehicle.set_wheel_rest_length(rest_spinner.value)
+	vehicle.set_wheel_roll_influence(roll_influence_spinner.value)
+	vehicle.set_suspension_stiffness(stiffness_spinner.value)
+	vehicle.set_suspension_max_force(max_force_spinner.value)
+	vehicle.set_suspension_travel(travel_spinner.value)
+	vehicle.set_damping_compression(damping_compression_spinner.value)
+	vehicle.set_damping_relaxation(damping_relaxation_spinner.value)
 
-	balance_label_front.text = "%.2f" % forward_balance
-	balance_label_rear.text = "%.2f" % rear_balance
+	vehicle.set_grip_balance(balance_slider.value)
+	vehicle.set_friction_slip(friction_spinner.value)
 
-	for wheel in front_wheels:
-		wheel.wheel_friction_slip = friction_spinner.value * forward_balance
-	
-	for wheel in rear_wheels:
-		wheel.wheel_friction_slip = friction_spinner.value * rear_balance
+	update_balance_labels()
 
+func update_balance_labels():
+	balance_label_front.text = "%.2f" % vehicle.get_forward_balance()
+	balance_label_rear.text = "%.2f" % vehicle.get_rear_balance()
 
 func update_stats():
 	var speed_mps = vehicle.linear_velocity.length()
 	var speed_kph = speed_mps * 3.6
 	speed_label.text = "Speed: %d" % speed_kph
 	var wheel_info_text = ""
-	for wheel in front_wheels + rear_wheels:
+	for wheel in vehicle.front_wheels + vehicle.rear_wheels:
 		wheel_info_text += "%s RPM: %d\n" % [wheel.name, wheel.get_rpm()]
 	wheel_info_label.text = wheel_info_text
 
